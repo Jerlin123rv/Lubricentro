8fc0cd9393e6a2662f7c46b579f474b73e65e56d
Jerlin123rv <jerlinjimenezhoyos@gmail.com>
2024-11-11 13:04:29 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Dirección de correo a la que quieres enviar el mensaje
+    $destinatario = "jhoninga@lubricentro.org";
+
+    // Recoger los datos del formulario
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    // Asunto del correo
+    $asunto = "Nuevo mensaje de contacto";
+
+    // Cuerpo del correo
+    $cuerpo = "Nombre: $nombre\n";
+    $cuerpo .= "Email: $email\n";
+    $cuerpo .= "Mensaje: $mensaje\n";
+
+    // Encabezados
+    $headers = "From: $email";
+
+    // Enviar el correo
+    if (mail($destinatario, $asunto, $cuerpo, $headers)) {
+        echo "Mensaje enviado correctamente.";
+    } else {
+        echo "Error al enviar el mensaje.";
+    }
+}
+?>
